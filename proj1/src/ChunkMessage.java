99cd78b802c852fa675f840f58d4b5e0b091287b
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-03-12 09:58:45 +0000

    Implemented most messages

---
+++ proj1/src/ChunkMessage.java
@@ -0,0 +1,25 @@
+import java.net.InetSocketAddress;
+
+public class ChunkMessage extends Message {
+    private int chunkNo;
+    private final byte[] body;
+
+    public ChunkMessage(String version, int senderId, String fileId, int chunkNo, byte[] body, InetSocketAddress inetSocketAddress){
+        super(version, "PUTCHUNK", senderId, fileId, inetSocketAddress);
+
+        if(body == null) throw new NullPointerException("body");
+
+        this.chunkNo = chunkNo;
+        this.body = body;
+    }
+
+    public byte[] getBytes(){
+        byte[] header = super.getBytes();
+        byte[] chunkNo_bytes = (" " + chunkNo + "\r\n\r\n").getBytes();
+        byte[] ret = new byte[header.length + chunkNo_bytes.length + body.length];
+        System.arraycopy(header       , 0, ret, 0, header.length);
+        System.arraycopy(chunkNo_bytes, 0, ret, header.length, chunkNo_bytes.length);
+        System.arraycopy(body         , 0, ret, header.length + chunkNo_bytes.length, body.length);
+        return ret;
+    }
+}
---
+++ proj1/src/StoredMessage.java
@@ -0,0 +1,19 @@
+import java.net.InetSocketAddress;
+
+public class StoredMessage extends Message {
+    private int chunkNo;
+
+    public StoredMessage(String version, int senderId, String fileId, int chunkNo, InetSocketAddress inetSocketAddress){
+        super(version, "STORED", senderId, fileId, inetSocketAddress);
+        this.chunkNo = chunkNo;
+    }
+
+    public byte[] getBytes(){
+        byte[] header = super.getBytes();
+        byte[] chunkNo_bytes = (" " + chunkNo + "\r\n\r\n").getBytes();
+        byte[] ret = new byte[header.length + chunkNo_bytes.length];
+        System.arraycopy(header       , 0, ret, 0, header.length);
+        System.arraycopy(chunkNo_bytes, 0, ret, header.length, chunkNo_bytes.length);
+        return ret;
+    }
+}
