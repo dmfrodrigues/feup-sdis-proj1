8df2eeacf10ea555b9c023cb9aea54028326840b
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-04-07 00:22:53 +0100

    Some fixes to factory

--- proj1/src/MessageFactory.java
+++ proj1/src/MessageFactory.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class MessageFactory {
     public MessageFactory(){}
 
-    public Message factoryMethod(DatagramPacket datagramPacket){
+    public Message factoryMethod(DatagramPacket datagramPacket) throws ClassNotFoundException {
         byte[] data = Arrays.copyOf(datagramPacket.getData(), datagramPacket.getLength());
         InetSocketAddress inetSocketAddress = (InetSocketAddress) datagramPacket.getSocketAddress();
 
@@ -17,27 +17,35 @@ public class MessageFactory {
         String messageType = headerSplit[1];
         int senderId = Integer.parseInt(headerSplit[2]);
         String fileId = headerSplit[3];
-        Integer chunkNo = (headerSplit.length >= 5 ? Integer.parseInt(headerSplit[4]) : null);
-        Integer replicationDeg = (headerSplit.length >= 6 ? Integer.parseInt(headerSplit[5]) : null);
 
         // Messages without body
-        Message ret = switch (messageType) {
-            case "STORED" -> new StoredMessage(version, senderId, fileId, chunkNo, inetSocketAddress);
-            case "GETCHUNK" -> new GetchunkMessage(version, senderId, fileId, chunkNo, inetSocketAddress);
-            case "DELETE" -> new DeleteMessage(version, senderId, fileId, inetSocketAddress);
-            case "REMOVED" -> new RemovedMessage(version, senderId, fileId, chunkNo, inetSocketAddress);
-            default -> null;
+        switch (messageType) {
+            case "DELETE": return new DeleteMessage(version, senderId, fileId, inetSocketAddress);
+            default: break;
         };
-        if(ret != null) return ret;
+
+        int chunkNo = Integer.parseInt(headerSplit[4]);
+        switch (messageType) {
+            case "STORED": return new StoredMessage(version, senderId, fileId, chunkNo, inetSocketAddress);
+            case "GETCHUNK": return new GetchunkMessage(version, senderId, fileId, chunkNo, inetSocketAddress);
+            case "REMOVED": return new RemovedMessage(version, senderId, fileId, chunkNo, inetSocketAddress);
+            default: break;
+        };
 
         // Messages with body
         int bodyOffset = Utils.find_nth(data, new byte[]{'\r', '\n'}, 2)+2;
         byte[] body = Arrays.copyOfRange(data, bodyOffset, data.length);
-        ret = switch (messageType) {
-            case "PUTCHUNK" -> new PutchunkMessage(version, senderId, fileId, chunkNo, replicationDeg, body, inetSocketAddress);
-            case "CHUNK" -> new ChunkMessage(version, senderId, fileId, chunkNo, body, inetSocketAddress);
-            default -> null;
+        switch (messageType) {
+            case "CHUNK": return new ChunkMessage(version, senderId, fileId, chunkNo, body, inetSocketAddress);
+            default: break;
         };
-        return ret;
+
+        int replicationDeg = Integer.parseInt(headerSplit[5]);
+        switch (messageType) {
+            case "PUTCHUNK": return new PutchunkMessage(version, senderId, fileId, chunkNo, replicationDeg, body, inetSocketAddress);
+            default: break;
+        };
+
+        throw new ClassNotFoundException(messageType);
     }
 }
