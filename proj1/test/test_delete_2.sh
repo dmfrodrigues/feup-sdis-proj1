162190ce0475f27e5c452df7d176aadfa2953abf
BrenoAccioly <breno.accioly91@gmail.com>
2021-04-09 14:16:47 +0100

    Fix in tests

--- proj1/test/test_delete_2.sh
+++ proj1/test/test_delete_2.sh
@@ -45,28 +45,28 @@ ls
 rm -rf 1 2 3 4
 curl http://ftp.debian.org/debian/dists/jessie/main/source/Release -o source_Release  # 102B
 curl http://ftp.debian.org/debian/dists/jessie/Release             -o Release         # 77.3KB
-timeout $TIMEOUT java sdis.PeerDriver $VERSION 1 service1 $MC_ADDR $MC_PORT $MDB_ADDR $MDB_PORT $MDR_ADDR $MDR_PORT  & PID1=$!
-timeout $TIMEOUT java sdis.PeerDriver $VERSION 2 service2 $MC_ADDR $MC_PORT $MDB_ADDR $MDB_PORT $MDR_ADDR $MDR_PORT > /dev/null & PID2=$!
-timeout $TIMEOUT java sdis.PeerDriver $VERSION 3 service3 $MC_ADDR $MC_PORT $MDB_ADDR $MDB_PORT $MDR_ADDR $MDR_PORT > /dev/null & PID3=$!
-timeout $TIMEOUT java sdis.PeerDriver $VERSION 4 service4 $MC_ADDR $MC_PORT $MDB_ADDR $MDB_PORT $MDR_ADDR $MDR_PORT > /dev/null & PID4=$!
+timeout $TIMEOUT java PeerDriver $VERSION 1 service1 $MC_ADDR $MC_PORT $MDB_ADDR $MDB_PORT $MDR_ADDR $MDR_PORT  & PID1=$!
+timeout $TIMEOUT java PeerDriver $VERSION 2 service2 $MC_ADDR $MC_PORT $MDB_ADDR $MDB_PORT $MDR_ADDR $MDR_PORT > /dev/null & PID2=$!
+timeout $TIMEOUT java PeerDriver $VERSION 3 service3 $MC_ADDR $MC_PORT $MDB_ADDR $MDB_PORT $MDR_ADDR $MDR_PORT > /dev/null & PID3=$!
+timeout $TIMEOUT java PeerDriver $VERSION 4 service4 $MC_ADDR $MC_PORT $MDB_ADDR $MDB_PORT $MDR_ADDR $MDR_PORT > /dev/null & PID4=$!
 echo "Started peers with PIDs $PID1, $PID2, $PID3, $PID4"
 sleep 1
-timeout $TIMEOUT java sdis.TestApp service1 BACKUP source_Release 1
+timeout $TIMEOUT java TestApp service1 BACKUP source_Release 1
 sleep 2
-timeout $TIMEOUT java sdis.TestApp service1 BACKUP Release 1
+timeout $TIMEOUT java TestApp service1 BACKUP Release 1
 sleep 3
 
 kill $PID3 # Stops 3
 
-test "test2-01-1" "timeout $TIMEOUT java sdis.TestApp service1 DELETE source_Release" "echo"
+test "test2-01-1" "timeout $TIMEOUT java TestApp service1 DELETE source_Release" "echo"
 sleep 1
 FILE=3/storage/chunks/8C5A4F80497BC0C4719B9DCE7CCC75C36BCB3938A65FB65F7CC0CA0074279526
 test2 "test2-01-2" "$FILE"
 
-timeout $TIMEOUT java sdis.PeerDriver $VERSION 3 service3 $MC_ADDR $MC_PORT $MDB_ADDR $MDB_PORT $MDR_ADDR $MDR_PORT  & PID3=$!
+timeout $TIMEOUT java PeerDriver $VERSION 3 service3 $MC_ADDR $MC_PORT $MDB_ADDR $MDB_PORT $MDR_ADDR $MDR_PORT  & PID3=$!
 sleep 1
 
-test "test2-02-1" "timeout $TIMEOUT java sdis.TestApp service1 DELETE Release" "echo"
+test "test2-02-1" "timeout $TIMEOUT java TestApp service1 DELETE Release" "echo"
 sleep 2
 FILE=3/storage/chunks/14C33F2915CA0D86673BCF9A54BC42F73F8A31E0ED6B3EF0D203EAC500F9047D
 test2 "test2-02-2" "$FILE"
