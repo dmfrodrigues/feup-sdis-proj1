5ff0f8701edeb86926f9c37f5c5b28b4c50795f9
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-04-10 19:19:59 +0100

    Added queue

--- proj1/src/sdis/Runnables/BackupFileCallable.java
+++ proj1/src/sdis/Runnables/BackupFileCallable.java
@@ -4,12 +4,21 @@ import sdis.Messages.PutchunkMessage;
 import sdis.Peer;
 import sdis.Storage.FileChunkIterator;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class BackupFileCallable extends BaseProtocolCallable {
+    /**
+     * Maximum amount of chunk backup futures that can be running at a given time.
+     *
+     * This also serves the purpose of avoiding backup from taking too many threads.
+     * But most importantly, it is to conserve memory as each running chunk backup future requires its chunk while
+     * running, which can exhaust memory.
+     */
+    private final static int MAX_FUTURE_QUEUE_SIZE = 10;
+
     private final Peer peer;
     private final FileChunkIterator fileChunkIterator;
     private final int replicationDegree;
@@ -26,21 +35,29 @@ public class BackupFileCallable extends BaseProtocolCallable {
 
         peer.getFileTable().setFileDesiredRepDegree(fileChunkIterator.getFileId(), replicationDegree);
 
-        List<Future<Void>> futureList = new ArrayList<>();
+        Queue<Future<Void>> futureQueue = new LinkedList<>();
 
         for(int i = 0; i < n; ++i){
+            while(futureQueue.size() >= MAX_FUTURE_QUEUE_SIZE) {
+                try {
+                    futureQueue.remove().get();
+                } catch (InterruptedException | ExecutionException e) {
+                    System.err.println(fileChunkIterator.getFileId() + " | Aborting backup of file");
+                    e.printStackTrace();
+                    return null;
+                }
+            }
             byte[] chunk = fileChunkIterator.next();
             PutchunkMessage message = new PutchunkMessage(peer.getId(), fileChunkIterator.getFileId(), i, replicationDegree, chunk, peer.getDataBroadcastAddress());
             BackupChunkCallable backupChunkCallable = new BackupChunkCallable(peer, message, replicationDegree);
-            futureList.add(peer.getExecutor().submit(backupChunkCallable));
+            futureQueue.add(peer.getExecutor().submit(backupChunkCallable));
         }
-        for(int i = 0; i < n; ++i) {
-            Future<Void> f = futureList.get(i);
+        while(!futureQueue.isEmpty()) {
             try {
-                f.get();
+                futureQueue.remove().get();
             } catch (InterruptedException | ExecutionException e) {
+                System.err.println(fileChunkIterator.getFileId() + " | Aborting backup of file");
                 e.printStackTrace();
-                System.err.println("Aborting backup of file " + fileChunkIterator.getFileId());
                 return null;
             }
         }
